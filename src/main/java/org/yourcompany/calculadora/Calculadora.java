e233cbd948953847b1a95955187bdcf566796582
Oriol Virgili <orvi907@vidalibarraquer.net>
2025-10-24 12:33:52 +0200

    Calculadora

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -1,10 +1,11 @@
-
-
 package org.yourcompany.calculadora;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
- * @author Posa el teu nom
+ * @author Oriol Virgili Garrigós
  */
 
 
@@ -17,7 +18,15 @@ public class Calculadora {
         System.out.println("Nombre de dígits de 56789: " + nombreDigits(56789));
         System.out.println("Nombre de dígits de -101010: " + nombreDigits(-101010));
         System.out.println("Nombre de dígits de 0: " + nombreDigits(0));
-
+        try {
+            int cares = numeroCares(100);
+            System.out.println("Nombre de cares: " + cares);
+            System.out.println("Nombre de creus: " + (100 - cares));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Operació interrompuda mentre es comptaven les cares.");
+        }
+        preuCinema(5,true,true);
         // Exemples de crida per al mètode sumaPrimersNumeros
         // System.out.println("Suma dels primers 5 números: " + sumaPrimersNumeros(5));
         // System.out.println("Suma dels primers 10 números: " + sumaPrimersNumeros(10));
@@ -60,7 +69,57 @@ public class Calculadora {
             comptador++;
         }
         return comptador;
+    } 
+    public static int numeroCares(int repeticions) throws InterruptedException {
+       Random rand = new Random();
+        int num;
+        byte contador=0;
+        byte cara=0;
+        //byte creu=0;
+        
+        while (contador < 100) { 
+            num = rand.nextInt(2); 
+
+            if (num == 0) {
+                String moneda = "cara";
+                cara++;
+                System.out.println("Ha sortit " + moneda + ".");
+            } else {
+                String moneda = "creu";
+                //creu++;
+                System.out.println("Ha sortit " + moneda + ".");
+            }
+
+            contador++;
+            Thread.sleep(500); // pausa de 500 mil·lisegons
+        }
+
+        System.out.println("Fi de les 100 tirades.");
+        return cara;
+        
     }
 
+public static double preuCinema(double preuBase, boolean capSetmana, boolean carnetJove) {
+    Scanner teclat = new Scanner(System.in);
+    //System.out.println("Indica de quin tipus de dia disposa: (Laborable=L, Cap de setmana=C  )");
+    //char tipus_entrada = teclat.next().charAt(0);
+    //System.out.println("Disposa de carnet Jove (S/N) ");
+    //char descompte = teclat.next().charAt(0);
+    double entrada=preuBase;
+    if (capSetmana==true) {
+        entrada=entrada*0.15;
+    }
+    if (carnetJove==true) {
+        entrada=entrada*0.10;
+    }
+    System.out.println("L'entrada del cinema val " + entrada + " €.");
+    teclat.close();
+    return entrada;
+}
 
 }
+
+
+    
+
+
